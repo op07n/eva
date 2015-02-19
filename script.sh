a1de70da8ec50c71cc46b3f7bfdf72a3d41a95fa
avidalh <avidalh@gmail.com>
2015-02-19 09:55:59 +0000

    updated statistics.csv and script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ date
 #find /Volumes/UNTITLED\ 1/grabaciones/tenerife/smr/11/1*/* -name *.gps | xargs -I{} ./AstTool_0300.py -f{} -d1 -b
 #find ../../Asterix/SMR/06/16/* -name *.gps | xargs -I{} ./AstTool_0400.py -f{} -d1 -b
 
-find /Volumes/UNTITLED\ 1/grabaciones/tenerife/smr/2014/12/*/* -name *.gps | xargs -I{} ./eva_0500.py -f{} -l1 -b -d -m -i 
+find /Volumes/UNTITLED\ 1/grabaciones/tenerife/smr/2015/01/*/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -m -i 
 
 echo Batch Analisys Ends...
 date
