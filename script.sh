bc0008fe7188b49bbf0283b1ddd6f36c9cefac02
avidalh <avidalh@gmail.com>
2015-08-28 12:33:23 +0100

    Starting after a long stopping time due to Udacity... ;-)

--- script.sh
+++ script.sh
@@ -6,15 +6,16 @@ date
 #find ../../Asterix/SMR/06/16/* -name *.gps | xargs -I{} ./AstTool_0400.py -f{} -d1 -b
 
 #find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/04/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
-find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/05/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
-find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/06/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
-find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/07/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
-find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/08/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
-find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/09/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
-find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/10/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
-find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/11/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
-find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/12/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
-
+#find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/13/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
+# find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/14/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
+# find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/15/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
+# find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/16/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
+# find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/17/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
+find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/25/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
+find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/26/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
+find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/27/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
+find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/28/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
+find /Volumes/16GB/grabaciones/tenerife/smr/2015/03/29/* -name *.gps | xargs -I{} ./eva.py -f{} -l1 -b -d -i 
 echo Batch Analisys Ends...
 date
 
